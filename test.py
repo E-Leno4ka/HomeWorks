57462197e5b76a2396ade5b8c03f75a0fb6175c0
E-Leno4ka <egrechman@gmail.com>
2025-08-21 19:30:31 +0300

    ++

---
+++ test.py
@@ -0,0 +1,39 @@
+from tkinter import *
+from tkinter import simpledialog as sd
+from tkinter import messagebox as mb
+import datetime
+#import pygame
+import time
+
+t = None
+music = False  # Переменная для отслеживания проигрывания музыки
+
+def set():
+    global t
+    rem = sd.askstring("Время напоминания", "Введите время в формате ЧЧ:ММ (24-часовой формат)")
+    if rem:
+        try:
+            hour = int(rem.split(":")[0])
+            minute = int(rem.split(":")[1])
+            if hour < 0 or hour > 23 or minute < 0 or minute > 59:
+                mb.showerror("Ошибка", "Некорректно задано время! \nВведите \nчасы от 00 до 23 \nминуты от 00 до 59")
+                return
+            now = datetime.datetime.now()
+            dt = now.replace(hour=hour, minute=minute, second=0, microsecond=0)
+            t = dt.timestamp()
+            label.config(text=f"Напоминание установлено на: {hour:02}:{minute:02}")
+        except ValueError:
+            mb.showerror("Ошибка", "Неверный формат времени")
+
+window = Tk()
+window.title("Напоминание")
+
+label = Label(text="Установите напоминание", font=("Arial", 14))
+label.pack(pady=10)
+
+set_button = Button(text="Установить напоминание", command=set)
+set_button.pack(pady=10)
+
+
+
+window.mainloop()
